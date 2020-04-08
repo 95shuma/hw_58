7b5695c5ae9e2d1322e441dc7355df2151b0070a
95shuma <95shuma@gmail.com>
2020-04-08 22:09:34 +0600

    some change

--- script.js
+++ script.js
@@ -48,66 +48,7 @@ function addCommentElement(elem) {
 	document.getElementById("comment").appendChild(elem);
 }
 
-function createPostElement(post) {
-	let elem = document.createElement(`div`);
-	elem.innerHTML = `
-          <!-- image block start -->
-          <div>
-            <img class="d-block w-100" src="${post.imageURL}" alt="Post image">
-          </div>
-          <!-- image block end -->
-          <div class="px-4 py-3">
-            <!-- post reactions block start -->
-            <!--<div class="d-flex justify-content-around">
-              <span class="h1 mx-2 text-danger">
-                <i class="fas fa-heart"></i>
-              </span>-->
-              <span class="h1 mx-2 muted like">
-                <i class="far fa-heart"></i>
-              </span>
-              <span class="h1 mx-2 muted">
-                <i class="far fa-comment"></i>
-              </span>
-              <span class="mx-auto"></span>
-              <!--<span class="h1 mx-2 muted">
-                <i class="far fa-bookmark"></i>
-              </span>-->
-              <span class="h1 mx-2 muted">
-                <i class="fas fa-bookmark"></i>
-              </span>
-            </div>
-            <!-- post reactions block end -->
-            <hr>
-            <!-- post section start -->
-            <div>
-              <p>${post.description}</p>
-              </div>
-            <!-- post section end -->
-            <hr>
-            <!-- comments section start -->
-            <div>
-              <div class="py-2 pl-3">
-                <a href="#" class="muted">someusername</a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ad est cumque nulla voluptatem enim voluptas minima illum quis! Voluptatibus dolorem minus tempore aliquid corrupti nesciunt, obcaecati fuga natus officiis.</p>
-              </div>
-              <div class="py-2 pl-3">
-                <a href="#" class="muted">someusername</a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ad est cumque nulla voluptatem enim voluptas minima illum quis! Voluptatibus dolorem minus tempore aliquid corrupti nesciunt, obcaecati fuga natus officiis.</p>
-              </div>
-            </div>
-            <!-- comments section end -->
-          </div>`;
-	let att = document.createAttribute("class");
-	att.value = "card my-3";
-	elem.setAttributeNode(att);
-	return elem;
-}
 
-let postElement = createPostElement(post);
-
-function addPostElement(elem) {
-	document.getElementById("posts").appendChild(elem);
-};
 
 window.addEventListener('load',function () {
     let like = document.getElementsByClassName("like")[0];
@@ -140,14 +81,77 @@ window.addEventListener('load',function () {
         }
     });
 
-    let bookmark = document.getElementsByClassName("bookmark")[0];
-    bookmark.addEventListener('click',function () {
-        if(bookmark.children[0].classList.contains("far")){
-            bookmark.children[0].classList.remove("far");
-            bookmark.children[0].classList.add("fas");
-        } else {
-            bookmark.children[0].classList.remove("fas");
-            bookmark.children[0].classList.add("far");
-        }
-    });
+    let bookmark = document.getElementsByClassName("bookmark");
+    for (let i=0; i<bookmark.length;i++) {
+        bookmark[i].addEventListener('click', function () {
+            if (bookmark[i].children[0].classList.contains("far")) {
+                bookmark[i].children[0].classList.remove("far");
+                bookmark[i].children[0].classList.add("fas");
+            } else {
+                bookmark[i].children[0].classList.remove("fas");
+                bookmark[i].children[0].classList.add("far");
+            }
+        });
+    }
 });
+
+function createPostElement(post) {
+    let elem = document.createElement(`div`);
+    elem.innerHTML = `
+          <!-- image block start -->
+          <div>
+            <img class="d-block w-100" src="${post.imageURL}" alt="Post image">
+          </div>
+          <!-- image block end -->
+          <div class="px-4 py-3">
+            <!-- post reactions block start -->
+            <div class="d-flex justify-content-around">
+              <!--<span class="h1 mx-2 text-danger">
+                <i class="fas fa-heart"></i>
+              </span>-->
+              <span class="h1 mx-2 muted like">
+                <i class="far fa-heart"></i>
+              </span>
+              <span class="h1 mx-2 muted">
+                <i class="far fa-comment"></i>
+              </span>
+              <span class="mx-auto"></span>
+              <span class="h1 mx-2 muted bookmark">
+                <i class="far fa-bookmark"></i>
+              </span>
+              <!--<span class="h1 mx-2 muted">
+                <i class="fas fa-bookmark"></i>
+              </span>-->
+            </div>
+            <!-- post reactions block end -->
+            <hr>
+            <!-- post section start -->
+            <div>
+              <p>${post.description}</p>
+              </div>
+            <!-- post section end -->
+            <hr>
+            <!-- comments section start -->
+            <div>
+              <div class="py-2 pl-3">
+                <a href="#" class="muted">someusername</a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ad est cumque nulla voluptatem enim voluptas minima illum quis! Voluptatibus dolorem minus tempore aliquid corrupti nesciunt, obcaecati fuga natus officiis.</p>
+              </div>
+              <div class="py-2 pl-3">
+                <a href="#" class="muted">someusername</a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ad est cumque nulla voluptatem enim voluptas minima illum quis! Voluptatibus dolorem minus tempore aliquid corrupti nesciunt, obcaecati fuga natus officiis.</p>
+              </div>
+            </div>
+            <!-- comments section end -->
+          </div>`;
+    let att = document.createAttribute("class");
+    att.value = "card my-3";
+    elem.setAttributeNode(att);
+    return elem;
+}
+
+let postElement = createPostElement(post);
+
+function addPostElement(elem) {
+    document.getElementById("posts").appendChild(elem);
+};
